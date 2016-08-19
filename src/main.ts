622b10f233b9238b00dcfb883ba19a880fae18ca
Eyjolfur Vatne <eyjolfur.vatne@live.no>
2016-08-19 11:26:06 +0200

    feat(init): commit files

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+
+import 'materialize-css/bin/materialize.css';
+import '../styles/styles.css';
+
+// comment out if you don't want a Promise polyfill (remove also from webpack.config.js)
+import * as Bluebird from 'bluebird';
+Bluebird.config({ warnings: false });
+
+import 'jquery';
+import 'materialize-css';
+import {Aurelia} from 'aurelia-framework';
+
+export async function configure(aurelia: Aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .developmentLogging()
+    .plugin('aurelia-materialize-bridge', b => b.useAll());
+
+  // Uncomment the line below to enable animation.
+  // aurelia.use.plugin('aurelia-animator-css');
+  // if the css animator is enabled, add swap-order="after" to all router-view elements
+
+  // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
+  // aurelia.use.plugin('aurelia-html-import-template-loader')
+
+  await aurelia.start();
+  aurelia.setRoot('app');
+
+  // if you would like your website to work offline (Service Worker),
+  // install and enable the @easy-webpack/config-offline package in webpack.config.js and uncomment the following code:
+  /*
+  const offline = await System.import('offline-plugin/runtime');
+  offline.install();
+  */
+}
